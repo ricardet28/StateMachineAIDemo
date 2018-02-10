8df0d03ec9f47a744423992d714fa2e9363456c8
Rickyy <lacajadericky@gmail.com>
2018-02-10 12:48:44 +0100

    player movement done

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Vector3 CaptureAxisMovement()
+    {
+        Vector3 captureMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        return captureMovement;
+    }
+
+    public Vector3 CaptureMouseInput()
+    {
+        return Input.mousePosition;
+
+
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speedMovement = 5f;
+    public float speedRotation = 5f;
+
+    Rigidbody rb;
+    PlayerInput _playerInput;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+
+        floorMask = LayerMask.GetMask("Floor");
+        rb = GetComponent<Rigidbody>();
+        _playerInput = GetComponent<PlayerInput>();
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move(_playerInput.CaptureAxisMovement());
+        Rotate(_playerInput.CaptureMouseInput());
+	}
+
+    void Move(Vector3 value)
+    {
+        rb.MovePosition(transform.position + value * speedMovement * Time.deltaTime);
+    }
+    void Rotate(Vector3 value)
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(value);
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse.normalized);
+            rb.MoveRotation(newRotation);
+        }
+        
+    }
+    
+}
